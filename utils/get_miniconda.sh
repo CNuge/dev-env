1a1e16e31e58e94837bef1d901015836f040000d
CNuge <camnugent@gmail.com>
2025-06-19 11:38:57 -0300

    miniforge to latest

--- utils/get_miniconda.sh
+++ utils/get_miniconda.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget -O ~/miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
--- utils/get_miniforge.sh
+++ utils/get_miniforge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O ~/miniforge.sh wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-24.11.2-1-Linux-x86_64.sh
+wget -O ~/miniforge.sh wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
